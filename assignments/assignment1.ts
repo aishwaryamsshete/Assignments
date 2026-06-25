8252d23b5549b2e838b8be0bd26beb84a1f99256
New <new@News-MacBook-Pro.local>
2026-06-25 23:28:50 +0530

    “Assignment1”

--- assignments/assignment1.ts
+++ assignments/assignment1.ts
@@ -1,4 +1,86 @@
-// Temp
+// 1. Temperature of a city in degrees Celsius: 25.5
 
 let temp: number=22.5;
-console.log(`tempea:${temp}`)
+console.log(`Temperature of a city in degrees Celsius:${temp}`)
+
+//2. Whether a customer has placed an order: true or false
+
+let hasplacedOrder: boolean=true;
+console.log("Customer hase placed an order:",hasplacedOrder)
+
+
+//3. Person's phone number: "123-456-7890"
+let phoneNumber: string="123-456-7890"
+console.log("Phone Number:",phoneNumber )
+
+
+//4. Amount of money in a customer's bank account: 1000.50
+let bankAccountBalance: number=1000.50
+console.log("Amount of money in a customer's bank account:",bankAccountBalance)
+
+
+//5. Person's email address: "john.doe@example.com"
+let emailAddress: string="john.doe@example.com"
+console.log("Person's email address:",emailAddress)
+
+
+//6. Coordinates of a location (latitude, longitude): 37.7749, -122.4194
+let coordinates: number[]=[37.7749, -122.4194]
+console.log("Coordinates of a location (latitude, longitude):","+ coordinates[0] +","+ coordinates[1] +")
+
+//7. Person's marital status: true or false
+let status: boolean= false
+console.log("Person's marital status: ",status)
+
+//8. Person's occupation: "Software Engineer"
+let occupation: string="Software Engineer"
+console.log("Person's occupation:",occupation)
+
+//9. Person's favourite colour: "Blue"
+let favouriteColour: string="Blue"
+console.log("Person's favourite colour:",favouriteColour)
+
+//10.Current year: 2023
+let year: number=2023
+console.log("Current year:",year)
+
+//11.Number of followers on a social media platform: 1,000,000
+let followers: string="1,000,000"
+console.log("Number of followers on a social media platform:",followers)
+
+//12.Rating of a movie: 7.5
+let movieRating: number=7.5
+console.log("Rating of a movie:",movieRating)
+
+//13.Person's blood type: 'A'
+let bloodType: string='A'
+console.log("Person's blood type: ",bloodType)
+
+//14.Title of a book: "To Kill a Mockingbird"
+let bookTitle: string="To Kill a Mockingbird"
+console.log("Number of employees in a company:",bookTitle)
+
+//15.Number of employees in a company: 500
+let numberOfEmployees: number=500
+console.log("Number of employees in a company:",numberOfEmployees)
+
+
+//16.Time of an event: 2:30 PM
+let event: string="2:30 PM"
+console.log("Name of a country:",event)
+
+//17.Name of a country: "United States"
+let country: string="United States"
+console.log("Name of a country:",country)
+
+//18.Person's eye color: "Brown"
+let eyecolor:string="Brown"
+console.log("Person's eye color:",eyecolor)
+
+//19.Person's birthplace: "New York City"
+let birthplace: string="New York City"
+console.log("Person's Birthplace:",birthplace)
+
+//20. Distance between two cities: 200.5
+let distance: number=200.5
+console.log("Distance between two cities:",distance)
