14bbe1d7d96833cc0f342d681d756a9c9b6070d6
New <new@News-MacBook-Pro.local>
2026-07-01 20:16:12 +0530

    Assignment4

--- assignments/assignment4.ts
+++ assignments/assignment4.ts
@@ -17,3 +17,44 @@ Loops and conditional statements
 4. If any transaction limit exceeds +/- 10000 then print the message “Suspicious credit/ debit
 Transaction with Amount” and also print total number of suspicious transactions*/
 
+const transaction: number[]=[50000, -2000, 3000, -15000, -200, -300, 4000, -3000] 
+
+let totalCredits: number=0;
+let totalDebits: number=0;
+let totalAmountCredited: number=0;
+let totalAmountDebited: number=0;
+//let remainingAmount: number=0;
+let suspiciousTransaction: number=0;
+
+for (let i=0; i<transaction.length; i++){
+    if (transaction[i]!>0){
+        totalCredits++;
+        totalAmountCredited += transaction[i]!;
+        //remainingAmount += transaction[i]!;
+        if (transaction[i]!>10000){
+            console.log(`Suspicious credit: ${transaction[i]}`);
+            suspiciousTransaction++;
+    }
+} else 
+    {
+    totalDebits++;
+    totalAmountDebited -= transaction[i]!;
+    //remainingAmount += transaction[i]!;
+        if (transaction[i]!< -10000){
+            console.log(`Suspicious debit: ${transaction[i]}`);
+            suspiciousTransaction++;
+
+}
+}
+}
+
+let remainingAmount = totalAmountCredited -totalAmountDebited
+
+
+console.log(" Total number of credit transactions:",totalCredits)
+console.log(" Total number of debit transactions:",totalDebits)
+console.log(" Total Amount Credited:",totalAmountCredited)
+console.log(" Total Amount debited:",totalAmountDebited)
+console.log(" Total Remaining Amount:",remainingAmount)  
+console.log(" Total Suspicious Transactions:",suspiciousTransaction)         
+        
